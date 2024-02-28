332f64a87956a0d24212d6533c71e98e9167948e
bietdoikiem <minhlucky2408@gmail.com>
2024-02-28 23:34:10 +0700

    initial commit

---
+++ algorithms/rabin_karp.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    cout << "Welcome to Rabin-Karp algorithm!" << endl;
+    return 0;
+}
---
+++ leetcode/weekly_contest_382/c_alice_and_bob_playing_flower_game.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() { 
+    int gcdRes = __gcd(1, 5);
+    cout << "gcdRes: " << gcdRes << endl;
+    return 0;
+}
+
