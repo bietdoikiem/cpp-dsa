280ea03fae94de02b3ed77fdd2957de4ddd94e2f
bietdoikiem <minhlucky2408@gmail.com>
2024-04-01 12:03:53 +0700

    feat: add hash function for multiple integers

---
+++ algo/hash_multiple_ints.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+ll get_hash(int i, int j, int k, int l) {
+    ll key = i + 1e2 * j + 1e4 * k + 1e6 * l;
+    return key;
+}
+
+int main() {
+    return 0;
+}
