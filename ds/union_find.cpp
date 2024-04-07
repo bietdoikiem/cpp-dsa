960b024a3561c51a4cacaf03ce0b585c9ec72878
bietdoikiem <minhlucky2408@gmail.com>
2024-04-07 16:08:09 +0700

    feat: add UnionFind data structure

---
+++ ds/union_find.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class UnionFind {
+public:
+    vector<int> par, sz;
+
+    UnionFind(int n) {
+        par.assign(n, 0);
+        for (int i = 0; i < n; i++) par[i] = i;
+        sz.assign(n, 1);
+    }
+
+    int find(int v) {
+        if (v == par[v]) return v;
+        return par[v] = find(par[v]);
+    }
+
+    void unite(int x, int y, int w) {
+        x = find(x), y = find(y);
+        if (x == y) {
+            return;
+        };
+        if (sz[x] < sz[y]) swap(x, y);
+        par[y] = par[x];
+        sz[x] += sz[y];
+    }
+};
+
+int main() {
+    UnionFind uf(10);
+    return 0;
+}
