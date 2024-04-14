b5638c9e402978b366d484b40e137688b1cf1e23
bietdoikiem <minhlucky2408@gmail.com>
2024-04-14 22:41:28 +0700

    chore: add print template

---
+++ template/print.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+ostream& operator<<(ostream& os, pair<int, int> const& x) {
+  os << x.first << ", " << x.second;
+  return os;
+}
+
+template <class T>
+ostream& operator<<(ostream& os, vector<T> const& x) {
+  os << "[ ";
+  for (auto y : x) os << y << " ";
+  return os << "]";
+}
+
+
+int main() {
+    return 0;
+}
